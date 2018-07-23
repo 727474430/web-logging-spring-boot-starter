f4d537bcbbb95d4f636fc76dbc00e85c1c7a95fe
Wang Liang <727474430@qq.com>
2018-07-23 15:38:06 +0800

    modify: request parameters query way

--- src/main/java/com/raindrop/aop/WebLoggingAop.java
+++ src/main/java/com/raindrop/aop/WebLoggingAop.java
@@ -18,6 +18,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
+import java.util.Enumeration;
 
 /**
  * @name: com.raindrop.aop.WebLoggingAop.java
@@ -30,134 +31,140 @@ import java.lang.reflect.Method;
 @Configuration
 public class WebLoggingAop {
 
-	private static Logger logger = LoggerFactory.getLogger(WebLogging.class);
+    private static Logger logger = LoggerFactory.getLogger(WebLogging.class);
 
-	private ThreadLocal<Long> time = new ThreadLocal();
+    private ThreadLocal<Long> time = new ThreadLocal();
 
-	/** need to exclude path */
-	private String excludePath;
-	/** need to print request headers */
-	private String printHeader;
+    /**
+     * need to exclude path
+     */
+    private String excludePath;
+    /**
+     * need to print request headers
+     */
+    private String printHeader;
 
-	public WebLoggingAop(String excludePath, String printHeader) {
-		this.excludePath = excludePath;
-		this.printHeader = printHeader;
-	}
+    public WebLoggingAop(String excludePath, String printHeader) {
+        this.excludePath = excludePath;
+        this.printHeader = printHeader;
+    }
 
-	public WebLoggingAop() { }
+    public WebLoggingAop() {
+    }
 
-	@Pointcut("@annotation(com.raindrop.anno.WebLogging)")
-	public void pointcut() { }
+    @Pointcut("@annotation(com.raindrop.anno.WebLogging)")
+    public void pointcut() {
+    }
 
-	@Before(value = "pointcut()")
-	public void before(JoinPoint joinPoint) {
-		ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-		HttpServletRequest request = requestAttributes.getRequest();
+    @Before(value = "pointcut()")
+    public void before(JoinPoint joinPoint) {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
 
-		if (isExcludePath(request.getRequestURI())) {
-			return;
-		}
+        if (isExcludePath(request.getRequestURI())) {
+            return;
+        }
 
-		try {
-			time.set(System.currentTimeMillis());
-			logger.info("=============Request Payload Start============");
-			logger.info("Request Url: {}", request.getRequestURL().toString());
-			logger.info("Request Content-Type: {}", request.getHeader("Content-Type"));
-			logger.info("Request Method: {}", request.getMethod());
-			logger.info("Request Headers: {}", getHeaders(request, printHeader));
-			logger.info("Request Description: {}", getDescription(joinPoint));
-			logger.info("Request Payload: {}", getRequestParameter(joinPoint));
-			logger.info("=============Request Payload End==============");
-		} catch (Exception e) {
-			logger.error("WebLoggingAop Request Parameter Parse Error: {}", e.getMessage());
-		}
-	}
+        try {
+            time.set(System.currentTimeMillis());
+            logger.info("=============Request Payload Start============");
+            logger.info("Request Url: {}", request.getRequestURL().toString());
+            logger.info("Request Content-Type: {}", request.getHeader("Content-Type"));
+            logger.info("Request Method: {}", request.getMethod());
+            logger.info("Request Headers: {}", getHeaders(request, printHeader));
+            logger.info("Request Description: {}", getDescription(joinPoint));
+            logger.info("Request Payload: {}", getRequestParameter(request));
+            logger.info("=============Request Payload End==============");
+        } catch (Exception e) {
+            logger.error("WebLoggingAop Request Parameter Parse Error: {}", e.getMessage());
+        }
+    }
 
-	@AfterReturning(returning = "result", pointcut = "pointcut()")
-	public void afterReturning(Object result) {
-		ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-		HttpServletResponse response = requestAttributes.getResponse();
-		HttpServletRequest request = requestAttributes.getRequest();
+    @AfterReturning(returning = "result", pointcut = "pointcut()")
+    public void afterReturning(Object result) {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletResponse response = requestAttributes.getResponse();
+        HttpServletRequest request = requestAttributes.getRequest();
 
-		if (isExcludePath(request.getRequestURI())) {
-			return;
-		}
+        if (isExcludePath(request.getRequestURI())) {
+            return;
+        }
 
-		logger.info("=============Response Payload Start============");
-		logger.info("Response Status: {}", response.getStatus());
-		logger.info("Response Payload: {}", JSON.toJSONString(result));
-		logger.info("Request Time: [ {}ms ]", System.currentTimeMillis() - time.get());
-		logger.info("=============Response Payload End==============");
-	}
+        logger.info("=============Response Payload Start============");
+        logger.info("Response Status: {}", response.getStatus());
+        logger.info("Response Payload: {}", JSON.toJSONString(result));
+        logger.info("Request Time: [ {}ms ]", System.currentTimeMillis() - time.get());
+        logger.info("=============Response Payload End==============");
+    }
 
-	/**
-	 * Combination request parameters to json format
-	 *
-	 * @param joinPoint
-	 * @return
-	 */
-	private String getRequestParameter(JoinPoint joinPoint) {
-		JSONObject jsonObject = new JSONObject();
-		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-		String[] parameterNames = signature.getParameterNames();
-		Object[] args = joinPoint.getArgs();
-		for (int i = 0; i < args.length; i++) {
-			jsonObject.put(parameterNames[i], args[i]);
-		}
-		return jsonObject.toJSONString();
-	}
+    /**
+     * Combination request parameters to json format
+     *
+     * @param request
+     * @return
+     */
+    private String getRequestParameter(HttpServletRequest request) {
+        JSONObject jsonObject = new JSONObject();
+        Enumeration<String> parameterNames = request.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String key = parameterNames.nextElement();
+            String value = request.getParameter(key);
+            jsonObject.put(key, value);
+        }
+        return jsonObject.toJSONString();
+    }
 
-	/**
-	 * get request method description info
-	 *
-	 * @param joinPoint
-	 * @return
-	 * @throws ClassNotFoundException
-	 */
-	private String getDescription(JoinPoint joinPoint) {
-		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-		Method method = signature.getMethod();
-		WebLogging webLogging = method.getAnnotation(WebLogging.class);
-		return webLogging.description();
-	}
+    /**
+     * get request method description info
+     *
+     * @param joinPoint
+     * @return
+     * @throws ClassNotFoundException
+     */
+    private String getDescription(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        WebLogging webLogging = method.getAnnotation(WebLogging.class);
+        return webLogging.description();
+    }
 
-	/**
-	 * return specify headers info
-	 *
-	 * @param printHeader
-	 * @return
-	 */
-	private String getHeaders(HttpServletRequest request, String printHeader) {
-		JSONObject result = new JSONObject();
-		if (printHeader != null && !"".equals(printHeader)) {
-			String[] specifyHeaders = printHeader.split(";");
-			for (int i = 0; i < specifyHeaders.length; i++) {
-				String header = request.getHeader(specifyHeaders[i]);
-				if (header != null || !"".equals(header)) {
-					result.put(specifyHeaders[i], header);
-				}
-			}
-		}
-		return result.toJSONString();
-	}
+    /**
+     * return specify headers info
+     *
+     * @param printHeader
+     * @return
+     */
+    private String getHeaders(HttpServletRequest request, String printHeader) {
+        JSONObject result = new JSONObject();
+        if (printHeader != null && !"".equals(printHeader)) {
+            String[] specifyHeaders = printHeader.split(";");
+            for (int i = 0; i < specifyHeaders.length; i++) {
+                String header = request.getHeader(specifyHeaders[i]);
+                if (header != null || !"".equals(header)) {
+                    result.put(specifyHeaders[i], header);
+                }
+            }
+        }
+        return result.toJSONString();
+    }
 
-	/**
-	 * 如果在excludePath中则为排除url 不进行日志处理
-	 * if request uri is in the exclude path, return false otherwise return true
-	 *
-	 * @param requestURI
-	 * @return
-	 */
-	private boolean isExcludePath(String requestURI) {
-		if (excludePath != null && !"".equals(excludePath)) {
-			String[] excludePaths = this.excludePath.split(";");
-			for (int i = 0; i < excludePaths.length; i++) {
-				if (requestURI.indexOf(excludePaths[i]) != -1) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+    /**
+     * 如果在excludePath中则为排除url 不进行日志处理
+     * if request uri is in the exclude path, return false otherwise return true
+     *
+     * @param requestURI
+     * @return
+     */
+    private boolean isExcludePath(String requestURI) {
+        if (excludePath != null && !"".equals(excludePath)) {
+            String[] excludePaths = this.excludePath.split(";");
+            for (int i = 0; i < excludePaths.length; i++) {
+                if (requestURI.indexOf(excludePaths[i]) != -1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 }
